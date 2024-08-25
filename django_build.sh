661be366ee36396c69f0178d6d7d1af7073dd7ec
O'rex <55725272+RAGREON@users.noreply.github.com>
2024-08-25 10:12:46 +0545

    Create django_build.sh

---
+++ django_build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+python -m venv venv
+cd venv
+source Scripts/activate
+pip install django
+pip freeze > requirements.txt
+django-admin startproject mysite
+cd mysite
+python manage.py startapp polls
+cat <<'EOF' > mysite/urls.py
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+EOF
+
+cat <<'EOF' > polls/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    print(request)
+    return HttpResponse("Hello World!")
+EOF
+
+cat <<EOF > polls/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
+EOF
+
+python manage.py runserver
