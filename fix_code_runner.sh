fb4c9ddb91782458df35403b0190ec8c9a1115fb
ragreon809@gmail.com <ragreon809@gmail.com>
2024-08-26 10:50:38 +0545

    completed script

---
+++ fix_code_runner.sh
@@ -0,0 +1,7 @@
+username=$(whoami)
+
+path="/c/Users/$username/AppData/Roaming/Code/User/settings.json"
+
+cat $path
+
+curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/settings.json > $path
