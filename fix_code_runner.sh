080dcfd53db6b0f309a0972443ec410a126e2b72
ragreon809@gmail.com <ragreon809@gmail.com>
2024-08-26 10:57:45 +0545

    added some lines

--- fix_code_runner.sh
+++ fix_code_runner.sh
@@ -5,3 +5,7 @@ path="/c/Users/$username/AppData/Roaming/Code/User/settings.json"
 cat $path
 
 curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/settings.json > $path
+
+clear
+
+echo "updated $path successfully"
