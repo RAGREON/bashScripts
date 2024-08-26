c55faca37be9e6f6a58b3ffa3f10f1c33640cc55
ragreon809@gmail.com <ragreon809@gmail.com>
2024-08-26 14:10:11 +0545

    created separate folder for python scripts called python_scripts

--- django_script/build_django_project.sh
+++ django_script/build_django_project.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+# Function to overwrite / add python files to respective directories
+
+function add_file() {
+    # replacing the comma in the filename with a forward slash
+    # ',' -> '/'
+
+    local path=${1/,//}
+
+    # redirecting file content from actual path location to modifed path
+
+    cat "$1" > $path
+}
+
 # Creating a new directory to start our project
 
 mkdir django_project
@@ -30,3 +44,8 @@ python manage.py startapp polls
 # Migrating
 
 python manage.py migrate
+
+# Adding files to our project
+
+for file in ./python_scripts/*; do
+    add_file
