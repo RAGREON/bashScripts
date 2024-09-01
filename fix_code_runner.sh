948fdcf33e334e0da52e71a1a865431e9e28f996
ragreon809@gmail.com <ragreon809@gmail.com>
2024-09-01 06:43:37 +0545

    fixed issues

--- fix_code_runner.sh
+++ fix_code_runner.sh
@@ -3,7 +3,7 @@ username=$(whoami)
 path="/c/Users/$username/AppData/Roaming/Code/User"
 
 curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/settings.json > "$path/settings.json"
-curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/vs_code_code_runner_key_mapping > "$path/vs_code_code_runner_key_mapping"
+curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/vs_code_code_runner_key_mapping > "/bin/vs_code_code_runner_key_mapping"
 
 clear
 
