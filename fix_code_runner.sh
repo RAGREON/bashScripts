7810af0e44e0d707a74e8bf2e5bf78ef80965d23
ragreon809@gmail.com <ragreon809@gmail.com>
2024-09-01 06:45:47 +0545

    fixed issues

--- fix_code_runner.sh
+++ fix_code_runner.sh
@@ -5,6 +5,4 @@ path="/c/Users/$username/AppData/Roaming/Code/User"
 curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/settings.json > "$path/settings.json"
 curl -s https://raw.githubusercontent.com/RAGREON/bashScripts/main/vs_code_code_runner_key_mapping > "/bin/vs_code_code_runner_key_mapping"
 
-clear
-
 echo "updated $path successfully"
